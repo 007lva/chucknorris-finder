a4a2bf8300344574c0b737770bf4c87193988c24
007lva <lvargas@rankia.com>
2021-06-04 19:58:10 +0200

    Show all results when no query

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,12 +3,18 @@ class SearchesController < ApplicationController
     respond_to do |format|
       format.html
       format.js do
-        results = Result.paginate(page: params[:page], per_page: 10)
+        results = Result.by_query(params_filter[:query]).paginate(page: params[:page], per_page: 10)
         render(partial: 'list', locals: { results: results }, content_type: 'text/plain')
       end
     end
   end
 
   def create
+  end
+
+  private
+
+  def params_filter
+    params.require(:search).permit(:query)
   end
 end
