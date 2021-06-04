1bdcacbb04892e7e85b8b8f7db3fa1d12d6dd9ed
007lva <lvargas@rankia.com>
2021-06-04 18:31:02 +0200

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,112 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Result.create(
+  [
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "ay_M9alERUe2dHseAIPx_Q",
+      "url": "https://api.chucknorris.io/jokes/ay_M9alERUe2dHseAIPx_Q",
+      "value": "Google Maps broke when searching for Chuck Norris."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "1jgggc4rruety6zvlvb5ag",
+      "url": "https://api.chucknorris.io/jokes/1jgggc4rruety6zvlvb5ag",
+      "value": "Google won't search for Chuck Norris because it knows you don't find Chuck Norris, he finds you."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "hcakrd4frju3vd-jakidqq",
+      "url": "https://api.chucknorris.io/jokes/hcakrd4frju3vd-jakidqq",
+      "value": "If you Google search \"Chuck Norris getting his ass kicked\" you will generate zero results. It just doesn't happen."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "SUpwpd76Q4qKiN54sskmUg",
+      "url": "https://api.chucknorris.io/jokes/SUpwpd76Q4qKiN54sskmUg",
+      "value": "When GOOGLE denied Chuck Norris his own search engine, he persuaded them with a swift kick to the \"G\". http://chuckoogle.com"
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "U8LGladiTEmv-c8YRbBk-Q",
+      "url": "https://api.chucknorris.io/jokes/U8LGladiTEmv-c8YRbBk-Q",
+      "value": "Every Tuesday morning Chuck Norris goes crawling the whole of the Internet with his friends Googlebot and Slurpy."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "Cs2rnIX_SbatJjEhV_C5Iw",
+      "url": "https://api.chucknorris.io/jokes/Cs2rnIX_SbatJjEhV_C5Iw",
+      "value": "Google doesn't search for Chuck Norris. It knows you don't find Chuck Norris, Chuck Norris finds you."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "27OoaJtASF-N4uVLEzyLZw",
+      "url": "https://api.chucknorris.io/jokes/27OoaJtASF-N4uVLEzyLZw",
+      "value": "If you google \"What is the purpose of life?\", a picture of Chuck Norris pops up."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "0A8dy1d2RbeP3NOe52DuJw",
+      "url": "https://api.chucknorris.io/jokes/0A8dy1d2RbeP3NOe52DuJw",
+      "value": "In fact there is no Google satellite views, just Chuck Norris eyes that he lets you to use it."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "354cuy9-RvWteu4lC0M77g",
+      "url": "https://api.chucknorris.io/jokes/354cuy9-RvWteu4lC0M77g",
+      "value": "Garmin GPS Googles Chuck Norris for directions."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "DGSMwgzPQcCKfY8mJsCYrg",
+      "url": "https://api.chucknorris.io/jokes/DGSMwgzPQcCKfY8mJsCYrg",
+      "value": "Chuck Norris can hit you so hard that not even Google will be able to find you."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "CsUSLdhbRS6MNTYqm5MFBA",
+      "url": "https://api.chucknorris.io/jokes/CsUSLdhbRS6MNTYqm5MFBA",
+      "value": "The Icelandic word for \"Chuck Norris\" is pronounced \"google.\" No joke, google it."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "L7qM9T2aTrGPOnppvAVmFg",
+      "url": "https://api.chucknorris.io/jokes/L7qM9T2aTrGPOnppvAVmFg",
+      "value": "Google uses Chuck Norris as a search engine."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "qrAqR3GQR0OqQg1wmU5Haw",
+      "url": "https://api.chucknorris.io/jokes/qrAqR3GQR0OqQg1wmU5Haw",
+      "value": "Chuck Norris doesn't Google, he Chuckles"
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "MqOYPgyqRFyBJHmZaxxqIg",
+      "url": "https://api.chucknorris.io/jokes/MqOYPgyqRFyBJHmZaxxqIg",
+      "value": "Google queries Chuck Norris' brain for search results."
+    },
+    {
+      "query": "google",
+      "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+      "api_id": "N6Qi8AxRQEKhXXizqcgZ1Q",
+      "url": "https://api.chucknorris.io/jokes/N6Qi8AxRQEKhXXizqcgZ1Q",
+      "value": "Google asked Chuck Norris where to search."
+    }
+  ]
+)
